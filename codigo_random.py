41da513fbf40b3cd167b8dfcce6b72f1205bec31
Isaque <isaquealeixo@hotmail.com>
2023-09-17 00:24:35 -0300

    v_01

---
+++ codigo_random.py
@@ -0,0 +1,9 @@
+print("bem vindo ao teste de git do isaque")
+escolha = int(input("digite 1 para olee e 2 para olá: "))
+if escolha == 1:
+    escolha = "olee"
+elif escolha == 2:
+    escolha = "ola"
+else:
+    print("mensagem invalida")
+print(f'vc escolheu a opção {escolha}')
