8d51505acb501888923d935ffb1f8cacef5129f0
Isaque <isaquealeixo@hotmail.com>
2023-09-19 01:08:17 -0300

    agora pode o nome

--- codigo_random.py
+++ codigo_random.py
@@ -1,3 +1,6 @@
+def voce_E(nome):
+    print(f"você {nome} é uma das pessoas ja feitas aqui no tal planeta terra")
+
 print("bem vindo ao teste de git do isaque")
 escolha = int(input("digite 1 para olee e 2 para olá: "))
 if escolha == 1:
@@ -8,4 +11,6 @@ else:
     print("mensagem invalida")
     print('tá errado')
     print("teste_01")
+nome = input("agora fala seu nome ai: ")
+print(voce_E(nome))
 print(f'vc escolheu a opção {escolha + " oi"}')
